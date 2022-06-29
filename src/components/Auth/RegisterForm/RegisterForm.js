b99bbe75fd5c77ceaec1767b6975ca7af247aa88
angelhpascual <angelhpascual@pop-os.localdomain>
2022-06-29 19:12:01 +0200

    Scss for RegisterForm

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,48 @@
 import React from "react"
+import { Button, Icon, Form, Input } from "semantic-ui-react"
+import { auth } from "../../../utils/firebase"
 
-export default function RegisterForm() {
+import "./RegisterForm.scss"
+
+export default function RegisterForm(props) {
+  const onSubmit = () => {
+    console.log("Hello There")
+  }
+  const { setSelectedForm } = props
   return (
-    <div>
-      <h1>RegisterForm...</h1>
+    <div className="register-form">
+      <h1>Empieza a escuchar con una cuenta de Musicfy gratis</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Correo Electrónico"
+            icon="mail outline"
+          ></Input>
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            icon="eye"
+          ></Input>
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="text"
+            name="username"
+            placeholder="¿Cómo deberíamos llamarte?"
+            icon="user circle outline"
+          ></Input>
+        </Form.Field>
+        <Button type="submit">Continuar</Button>
+      </Form>
+      <div className="register-form__options">
+        <p>Volver</p>
+        <p>¿Ya tienes Musicfy</p> <span>Iniciar Sesión</span>
+      </div>
     </div>
   )
 }
